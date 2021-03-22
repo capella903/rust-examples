92b143f8ad2ada413ecb2aaf44c353c71d62ec2c
Capella <52690366+capella903@users.noreply.github.com>
2021-03-22 22:31:22 +0900

    Add authentication

--- yup-oauth2-examples/examples/installed_flow_usage.rs
+++ yup-oauth2-examples/examples/installed_flow_usage.rs
@@ -1,11 +1,28 @@
-use tokio::io::{self, AsyncReadExt};
+use std::path::PathBuf;
+use structopt::StructOpt;
+use yup_oauth2::{InstalledFlowAuthenticator, InstalledFlowReturnMethod};
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    path: PathBuf,
+}
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> io::Result<()> {
-    let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).await?;
-    let secret = yup_oauth2::parse_application_secret(buffer)?;
-    dbg!(secret);
+async fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+
+    let secret = yup_oauth2::read_application_secret(opt.path).await?;
+
+    let auth = InstalledFlowAuthenticator::builder(secret, InstalledFlowReturnMethod::Interactive)
+        .persist_tokens_to_disk("token.json")
+        .build()
+        .await?;
+
+    let scopes = &["https://www.googleapis.com/auth/calendar.readonly"];
+
+    let token = auth.token(scopes).await?;
+
+    dbg!(token);
 
     Ok(())
 }
